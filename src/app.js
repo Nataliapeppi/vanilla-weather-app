d5c979de1ba81883b0d923ad48d7c53a08916e6f
Natalia <ntl.peppi@gmail.com>
2022-10-06 21:10:00 +0200

    coordinates

--- src/app.js
+++ src/app.js
@@ -21,6 +21,37 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="forcast-date">${day}</div>
+    <img 
+    src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="37"/>
+    <div class="forecast-temperature">
+      <span class="forecast-temperarure-max"> 29° </span>
+      <span class="forecast-temperature/min"> 22° </span>
+  </div>
+  </div>;
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "3219daf46ee9aba018948be7a2409d4b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -40,6 +71,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "2c975c993eaf1e67699dced574209890";
