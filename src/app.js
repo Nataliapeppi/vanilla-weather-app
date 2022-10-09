065b9a0f6428caa5497759323a298a9db1291938
Natalia <ntl.peppi@gmail.com>
2022-10-09 15:06:14 +0200

    display 5 days forecast using API

--- src/app.js
+++ src/app.js
@@ -20,27 +20,42 @@ function formatDate(timestamp) {
   let day = days[date.getDate()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
-    <div class="forcast-date">${day}</div>
+    <div class="forcast-date">${formatDay(forecastDay.dt)}</div>
     <img 
-    src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="37"/>
+    src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" alt="" width="37"/>
     <div class="forecast-temperature">
-      <span class="forecast-temperarure-max"> 29° </span>
-      <span class="forecast-temperature/min"> 22° </span>
+      <span class="forecast-temperarure-max"> ${Math.round(
+        forecastDay.temp.max
+      )}° </span>
+      <span class="forecast-temperature/min"> ${Math.round(
+        forecastDay.temp.min
+      )}° </span>
   </div>
   </div>;
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
